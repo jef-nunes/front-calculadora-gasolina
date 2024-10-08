c1618a0a433fdb99d169d6180e1ee884e06d744a
Jeferson <159483664+jef-nunes@users.noreply.github.com>
2024-10-08 16:34:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const input3 = document.getElementById("input-3");
 const btnSend = document.getElementById("btn-send-form-0");
 
 // Eventos
+
+btnSend.addEventListener("click", calcularTotal());
 // Calcular total a ser gasto com gasolina em R$
 function calcularTotal()
 {
-    console.log(`Inputs\n1:${input1.value}, 2:${input2.value}, 3:${input3.value}`);
-        let total = Number(input1.value)/Number(input2.value)
-        total *= Number(input3.value);
-        console.log(total);
+    let a = Number(input1.value);
+    let b = Number(input2.value);
+    let c = Number(input3.value);
+    let total = (a/b)*c
+    alert('R$ '+total.toFixed(2))
 }
 
 // Vincular o evento de envio do formulario
-btnSend.addEventListener("click", calcularTotal());
